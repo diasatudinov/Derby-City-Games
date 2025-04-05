158ee9d8344bd3fa3d072a5f41c1e2c788ef12b2
Dias <dias.atudinov@gmail.com>
2025-04-05 18:17:48 +0500

    start

---
+++ Derby City Games/Derby City Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Derby City Games
+//
+//  Created by Dias Atudinov on 05.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Derby City Games/Derby City Games/Derby_City_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Derby_City_GamesApp.swift
+//  Derby City Games
+//
+//  Created by Dias Atudinov on 05.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Derby_City_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
