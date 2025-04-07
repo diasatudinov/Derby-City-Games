f5bfeda0c3df0994d9c968f99c09736239da4fbd
Dias <dias.atudinov@gmail.com>
2025-04-07 21:24:24 +0500

    Rules view

--- Derby City Games/Derby City Games/ModulesDC/Rules/RulesView.swift
+++ Derby City Games/Derby City Games/ModulesDC/Rules/RulesView.swift
@@ -1,15 +1,38 @@
-//
-//  RulesView.swift
-//  Derby City Games
-//
-//  Created by Dias Atudinov on 07.04.2025.
-//
-
 import SwiftUI
 
 struct RulesView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                HStack(alignment: .top) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+
+                    } label: {
+                        Image(.backIconDC)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: DeviceInfo.shared.deviceType == .pad ? 100:50)
+                    }
+                    Spacer()
+                    
+                    
+                    MoneyViewDC()
+                }.padding([.horizontal, .top])
+            }
+            Spacer()
+        }
+        .background(
+            Image(.rulesView)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            
+        )
     }
 }
 
