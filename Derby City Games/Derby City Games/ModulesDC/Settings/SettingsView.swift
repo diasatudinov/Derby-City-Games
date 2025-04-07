48445846790a02aaa099e99a841c4bce9fc6a2d4
Dias <dias.atudinov@gmail.com>
2025-04-07 21:24:44 +0500

    settings view add

--- Derby City Games/Derby City Games/ModulesDC/Settings/SettingsView.swift
+++ Derby City Games/Derby City Games/ModulesDC/Settings/SettingsView.swift
@@ -8,11 +8,109 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    @ObservedObject var viewModel: SettingsViewModelDC
+    private let languages = ["English", "German", "Spanish"]
+    @State private var langIndex = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack(spacing: 15) {
+                TextWithBorder(text: "Sound", font: .system(size: DeviceInfo.shared.deviceType == .pad ? 40:20, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 2)
+                
+                HStack {
+                    TextWithBorder(text: "Off", font: .system(size: DeviceInfo.shared.deviceType == .pad ? 40:20, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 2)
+                    
+                    Button {
+                        withAnimation {
+                            viewModel.soundEnabled.toggle()
+                        }
+                    } label: {
+                        
+                        Image(viewModel.soundEnabled ? .onDC : .offDC)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: DeviceInfo.shared.deviceType == .pad ? 72:36)
+                    }
+                    
+                    TextWithBorder(text: "On", font: .system(size: DeviceInfo.shared.deviceType == .pad ? 40:20, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 2)
+                }
+                
+                TextWithBorder(text: "Language", font: .system(size: DeviceInfo.shared.deviceType == .pad ? 40:20, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 2)
+                
+                langView()
+            }
+            
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        } label: {
+                            Image(.backIconDC)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: DeviceInfo.shared.deviceType == .pad ? 100:50)
+                        }
+                        Spacer()
+                        
+                        
+                        MoneyViewDC()
+                    }.padding([.horizontal, .top])
+                }
+                Spacer()
+            }
+        }
+        .background(
+            Image(.settingsBgDC)
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            
+        )
+    }
+    
+    @ViewBuilder func langView() -> some View {
+        
+        VStack {
+            HStack {
+                Button {
+                    if langIndex > 0 {
+                        langIndex -= 1
+                    }
+                } label: {
+                    Image(.arrowDC)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: DeviceInfo.shared.deviceType == .pad ? 62:31)
+                }
+                
+                Image("\(languages[langIndex])FlagDC")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: DeviceInfo.shared.deviceType == .pad ? 84:42)
+                Button {
+                    if langIndex < 2 {
+                        langIndex += 1
+                    }
+                } label: {
+                    Image(.arrowDC)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: DeviceInfo.shared.deviceType == .pad ? 62:31)
+                        .scaleEffect(x: -1, y: 1)
+                }
+            }
+            
+            TextWithBorder(text: languages[langIndex], font: .system(size: DeviceInfo.shared.deviceType == .pad ? 32:16, weight: .bold), textColor: .white, borderColor: .black, borderWidth: 2)
+        }
+        
     }
 }
 
 #Preview {
-    SettingsView()
+    SettingsView(viewModel: SettingsViewModelDC())
 }
